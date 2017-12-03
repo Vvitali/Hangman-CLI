0bd683ba431cd57189db1a4ba1bc2f9af2b7b6dc
Vitaliy Vorobyev <vvitali26@gmail.com>
2017-12-03 00:04:59 -0500

    basic constructors: word, letter

--- run.js
+++ run.js
@@ -1,3 +1,27 @@
 var DEBUG = true;
-DEBUG && console.log("run.js");
+DEBUG && console.log("Started:run.js");
 var inquirer = require("inquirer");
+
+
+function Word(wordd) {
+	this.word = wordd;
+	this.massive = [];
+	this.Initializing = ()=>{
+		for(var index in this.word){
+			console.log(this.word[index]);
+
+		}
+	}
+
+}
+
+function Letter(innerLetter){
+	this.state = "_";
+	this.letterToShow = innerLetter;
+	this.Activate = ()=>{
+		this.state = this.letterToShow;
+	} 
+}
+
+var word = new Word("mashina");
+word.Initializing();
